ec51b40a09f46622e6d830784a7008c35484d2d6
usama <usamaali052@gmail.com>
2022-11-28 13:22:48 +0500

    editing and deleting todo api done

--- routes/todos.ts
+++ routes/todos.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import { Todo } from '../models/todo'
 
-const todos:Todo[] = []
+let todos:Todo[] = []
 
 const router = Router()
 
@@ -16,6 +16,22 @@ router.post('/', (req, res, next) => {
     text: req.body.text
   }
   todos.push(newTodo)
+  res.status(201).json({message: 'Todo added', todo: newTodo, todos})
+})
+
+router.post('/todo/:todoId', (req, res, next) => {
+  const tid = req.params.todoId
+  const todoIndex = todos.findIndex(todo => todo.id === tid)
+  if(todoIndex >= 0) {
+    todos[todoIndex] = {id: tid, text: req.body.text}
+    return res.status(200).json({message: 'Updated todo', todos})
+  }
+  res.status(404).json({message: 'Could not find'})
+})
+
+router.delete('/todo/:todoId', (req, res, next) => {
+  todos = todos.filter(todoItem => todoItem.id !== req.params.todoId)
+  res.status(200).json({message: 'Todo deleted', todos})
 })
 
 export default router
